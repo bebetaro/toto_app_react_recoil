4d6f70c3f46ccf45c4c30b09c304e0d047edfc69
kurokawa <kurokawa@aptpod.co.jp>
2020-12-06 19:25:55 +0900

    feat(component): add molecules/add

---
+++ src/components/molecules/buttons/add/component.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { Button } from "../../../atoms/button";
+import add from "../../../../assets/images/icon-24-expansion.svg";
+import style from "./style.css";
+import { pageState } from "../../../../atoms";
+import { PAGE } from "../../../../constants";
+import { Image } from "../../../atoms/image";
+
+/**
+ * Add Component
+ */
+export const Add: React.FC = function Add() {
+  const setPage = useSetRecoilState(pageState);
+  const onClick = React.useCallback(() => {
+    setPage(PAGE.ADD);
+  }, []);
+  return (
+    <div className={style.root}>
+      <Button onClick={onClick}>
+        <Image src={add} alt={"Add Button"} />
+      </Button>
+    </div>
+  );
+};
