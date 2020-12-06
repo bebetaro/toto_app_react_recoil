9beb39922e1659262b34d2d4859999c58fa22a90
kurokawa <kurokawa@aptpod.co.jp>
2020-12-06 19:34:03 +0900

    feat(component): add page component

---
+++ src/components/page/page/index.ts
@@ -0,0 +1,7 @@
+export { Page } from './component'
+export type { Props } from './component'
+
+
+
+
+
---
+++ src/components/page/page/style.css.d.ts
@@ -0,0 +1,4 @@
+export const root: string;
+export const sort: string;
+export const list: string;
+export const form: string;
