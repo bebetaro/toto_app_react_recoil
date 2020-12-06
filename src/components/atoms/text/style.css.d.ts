ea185cbe17505e9c03b5a673d74c60b7a355013a
bebetaro <takumi.kurokawa.dev@gmail.com>
2020-12-06 19:25:34 +0900

    feat(component): add atoms/text

---
+++ src/components/atoms/text/style.css.d.ts
@@ -0,0 +1,7 @@
+export const root: string;
+export const small: string;
+export const medium: string;
+export const big: string;
+export const white: string;
+export const black: string;
+export const red: string;
