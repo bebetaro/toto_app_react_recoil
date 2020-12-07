5ef1941d1e354a65ae0b55a17eeff21cf3345fc8
kurokawa <kurokawa@aptpod.co.jp>
2020-12-07 13:54:34 +0900

    feat(container): add storage to make store data permanent

--- src/containers/organisms/form/container.tsx
+++ src/containers/organisms/form/container.tsx
@@ -4,10 +4,11 @@ import { Form } from "../../../components/organisms/form";
 import * as Atoms from "../../../atoms";
 import { ATOMS_FAMILY } from "../../../constants";
 import { useOnClick } from "./hooks";
+import { storage } from "../../../storage";
 
 export const FormContainer = (): JSX.Element => {
+  const id = useRecoilValue(Atoms.selectIdState);
   const page = useRecoilValue(Atoms.pageState);
-  const task = useRecoilValue(Atoms.taskSelector("edit"));
   const [taskName, setTaskName] = useRecoilState(
     Atoms.inputAtomFamily(ATOMS_FAMILY.TASK_NAME)
   );
@@ -20,14 +21,16 @@ export const FormContainer = (): JSX.Element => {
   // set initial value
   useEffect(() => {
     if (page === "update") {
-      setTaskName(task.taskName);
-      setDeadline(task.deadline);
+      storage.get(id).then((task) => {
+        setTaskName(task.taskName);
+        setDeadline(task.deadline);
+      });
     }
     return () => {
       setTaskName("");
       setDeadline("");
     };
-  }, []);
+  }, [id]);
 
   return (
     <Form
