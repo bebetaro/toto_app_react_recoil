5e27335507439f8edc84443ca6eacc26dd7dabb7
kurokawa <kurokawa@aptpod.co.jp>
2020-12-08 13:45:31 +0900

    feat(image): fix images sizes

--- src/components/atoms/image/component.tsx
+++ src/components/atoms/image/component.tsx
@@ -4,11 +4,21 @@ import style from "./style.css";
 export type Props = {
   src: string;
   alt: string;
+  height: number;
+  width: number;
 };
 
 /**
  * Image Component
  */
 export const Image: React.FC<Props> = React.memo(function Image(props) {
-  return <img className={style.root} src={props.src} alt={props.alt} />;
+  return (
+    <img
+      className={style.root}
+      src={props.src}
+      alt={props.alt}
+      height={props.height}
+      width={props.width}
+    />
+  );
 });
